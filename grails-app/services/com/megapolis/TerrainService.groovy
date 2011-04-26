8ec817c6357b696cbe30c49bfaa79313c475c974
blonboll <blonboll@seznam.cz>
2011-04-26 17:35:18 +0200

    default roads

--- grails-app/services/com/megapolis/TerrainService.groovy
+++ grails-app/services/com/megapolis/TerrainService.groovy
@@ -27,14 +27,198 @@ class TerrainService {
         (-30..30).each { x ->
             (-30..30).each { y ->
                 def b = null
-                if (x == 0 && y == 0)
-                    b = new Building(type: BuildingType.findByDirname('road-upleft'))
-
-                b?.init()
                 double rx = R[0] * x + R[1] * y;
                 double ry = R[2] * x + R[3] * y;
-
-                new Field(coordX: x, coordY: y, rx: rx, ry: ry, building: b).save()
+                if (x == 0 && y == 0){
+                    b = new Building(type: BuildingType.findByDirname('road-upleft'))
+                    b?.init()
+                    new Field(coordX: x, coordY: y, rx: rx, ry: ry, building: b).save()
+                }
+                if (x == 1 && y == 0){
+                    b = new Building(type: BuildingType.findByDirname('road-upleft'))
+                    b?.init()
+                    new Field(coordX: x, coordY: y, rx: rx, ry: ry, building: b).save()
+                }
+                if (x == 2 && y == 0){
+                    b = new Building(type: BuildingType.findByDirname('Tcrossroad-south'))
+                    b?.init()
+                    new Field(coordX: x, coordY: y, rx: rx, ry: ry, building: b).save()
+                }
+                if (x == 3 && y == 0){
+                    b = new Building(type: BuildingType.findByDirname('road-upleft'))
+                    b?.init()
+                    new Field(coordX: x, coordY: y, rx: rx, ry: ry, building: b).save()
+                }
+                if (x == 4 && y == 0){
+                    b = new Building(type: BuildingType.findByDirname('road-upleft'))
+                    b?.init()
+                    new Field(coordX: x, coordY: y, rx: rx, ry: ry, building: b).save()
+                }
+                if (x == -1 && y == 0){
+                    b = new Building(type: BuildingType.findByDirname('crossroad'))
+                    b?.init()
+                    new Field(coordX: x, coordY: y, rx: rx, ry: ry, building: b).save()
+                }
+                if (x == -1 && y == 1){
+                    b = new Building(type: BuildingType.findByDirname('road-upright'))
+                    b?.init()
+                    new Field(coordX: x, coordY: y, rx: rx, ry: ry, building: b).save()
+                }
+                if (x == -1 && y == 2){
+                    b = new Building(type: BuildingType.findByDirname('road-upright'))
+                    b?.init()
+                    new Field(coordX: x, coordY: y, rx: rx, ry: ry, building: b).save()
+                }
+                if (x == -1 && y == 3){
+                    b = new Building(type: BuildingType.findByDirname('road-upright'))
+                    b?.init()
+                    new Field(coordX: x, coordY: y, rx: rx, ry: ry, building: b).save()
+                }
+                if (x == -1 && y == 4){
+                    b = new Building(type: BuildingType.findByDirname('road-upright'))
+                    b?.init()
+                    new Field(coordX: x, coordY: y, rx: rx, ry: ry, building: b).save()
+                }
+                if (x == -1 && y == 5){
+                    b = new Building(type: BuildingType.findByDirname('road-south-west'))
+                    b?.init()
+                    new Field(coordX: x, coordY: y, rx: rx, ry: ry, building: b).save()
+                }
+                if (x == -2 && y == 5){
+                    b = new Building(type: BuildingType.findByDirname('road-upleft'))
+                    b?.init()
+                    new Field(coordX: x, coordY: y, rx: rx, ry: ry, building: b).save()
+                }
+                if (x == -3 && y == 5){
+                    b = new Building(type: BuildingType.findByDirname('road-upleft'))
+                    b?.init()
+                    new Field(coordX: x, coordY: y, rx: rx, ry: ry, building: b).save()
+                }
+                if (x == -1 && y == -1){
+                    b = new Building(type: BuildingType.findByDirname('road-upright'))
+                    b?.init()
+                    new Field(coordX: x, coordY: y, rx: rx, ry: ry, building: b).save()
+                }
+                if (x == -1 && y == -2){
+                    b = new Building(type: BuildingType.findByDirname('road-upright'))
+                    b?.init()
+                    new Field(coordX: x, coordY: y, rx: rx, ry: ry, building: b).save()
+                }
+                if (x == -1 && y == -3){
+                    b = new Building(type: BuildingType.findByDirname('road-upright'))
+                    b?.init()
+                    new Field(coordX: x, coordY: y, rx: rx, ry: ry, building: b).save()
+                }
+                if (x == -1 && y == -4){
+                    b = new Building(type: BuildingType.findByDirname('road-north-east'))
+                    b?.init()
+                    new Field(coordX: x, coordY: y, rx: rx, ry: ry, building: b).save()
+                }
+                if (x == 0 && y ==-4 ){
+                    b = new Building(type: BuildingType.findByDirname('road-upleft'))
+                    b?.init()
+                    new Field(coordX: x, coordY: y, rx: rx, ry: ry, building: b).save()
+                }
+                if (x == 1 && y ==-4 ){
+                    b = new Building(type: BuildingType.findByDirname('road-upleft'))
+                    b?.init()
+                    new Field(coordX: x, coordY: y, rx: rx, ry: ry, building: b).save()
+                }
+                if (x == 2 && y ==-4 ){
+                    b = new Building(type: BuildingType.findByDirname('Tcrossroad-north'))
+                    b?.init()
+                    new Field(coordX: x, coordY: y, rx: rx, ry: ry, building: b).save()
+                }
+                if (x == 3 && y ==-4 ){
+                    b = new Building(type: BuildingType.findByDirname('road-upleft'))
+                    b?.init()
+                    new Field(coordX: x, coordY: y, rx: rx, ry: ry, building: b).save()
+                }
+                if (x == 4 && y ==-4 ){
+                    b = new Building(type: BuildingType.findByDirname('road-upleft'))
+                    b?.init()
+                    new Field(coordX: x, coordY: y, rx: rx, ry: ry, building: b).save()
+                }
+                if (x == 5 && y ==-4 ){
+                    b = new Building(type: BuildingType.findByDirname('road-north-west'))
+                    b?.init()
+                    new Field(coordX: x, coordY: y, rx: rx, ry: ry, building: b).save()
+                }
+                if (x == 5 && y == -3){
+                    b = new Building(type: BuildingType.findByDirname('road-upright'))
+                    b?.init()
+                    new Field(coordX: x, coordY: y, rx: rx, ry: ry, building: b).save()
+                }
+                if (x == 5 && y == -2){
+                    b = new Building(type: BuildingType.findByDirname('road-upright'))
+                    b?.init()
+                    new Field(coordX: x, coordY: y, rx: rx, ry: ry, building: b).save()
+                }
+                if (x == 5 && y == -1){
+                    b = new Building(type: BuildingType.findByDirname('road-upright'))
+                    b?.init()
+                    new Field(coordX: x, coordY: y, rx: rx, ry: ry, building: b).save()
+                }
+                if (x == 5 && y == 0){
+                    b = new Building(type: BuildingType.findByDirname('road-south-west'))
+                    b?.init()
+                    new Field(coordX: x, coordY: y, rx: rx, ry: ry, building: b).save()
+                }
+                if (x == 2 && y == -3){
+                    b = new Building(type: BuildingType.findByDirname('road-upright'))
+                    b?.init()
+                    new Field(coordX: x, coordY: y, rx: rx, ry: ry, building: b).save()
+                }
+                if (x == 2 && y == -2){
+                    b = new Building(type: BuildingType.findByDirname('road-upright'))
+                    b?.init()
+                    new Field(coordX: x, coordY: y, rx: rx, ry: ry, building: b).save()
+                }
+                if (x == 2 && y == -1){
+                    b = new Building(type: BuildingType.findByDirname('road-upright'))
+                    b?.init()
+                    new Field(coordX: x, coordY: y, rx: rx, ry: ry, building: b).save()
+                }
+                if (x == -2 && y ==0 ){
+                    b = new Building(type: BuildingType.findByDirname('road-upleft'))
+                    b?.init()
+                    new Field(coordX: x, coordY: y, rx: rx, ry: ry, building: b).save()
+                }
+                if (x == -3 && y ==0 ){
+                    b = new Building(type: BuildingType.findByDirname('road-upleft'))
+                    b?.init()
+                    new Field(coordX: x, coordY: y, rx: rx, ry: ry, building: b).save()
+                }
+                if (x == -4 && y ==0 ){
+                    b = new Building(type: BuildingType.findByDirname('road-north-east'))
+                    b?.init()
+                    new Field(coordX: x, coordY: y, rx: rx, ry: ry, building: b).save()
+                }
+                if (x == -4 && y == 1){
+                    b = new Building(type: BuildingType.findByDirname('road-upright'))
+                    b?.init()
+                    new Field(coordX: x, coordY: y, rx: rx, ry: ry, building: b).save()
+                }
+                if (x == -4 && y == 2){
+                    b = new Building(type: BuildingType.findByDirname('road-upright'))
+                    b?.init()
+                    new Field(coordX: x, coordY: y, rx: rx, ry: ry, building: b).save()
+                }
+                if (x == -4 && y == 3){
+                    b = new Building(type: BuildingType.findByDirname('road-upright'))
+                    b?.init()
+                    new Field(coordX: x, coordY: y, rx: rx, ry: ry, building: b).save()
+                }
+                if (x == -4 && y == 4){
+                    b = new Building(type: BuildingType.findByDirname('road-upright'))
+                    b?.init()
+                    new Field(coordX: x, coordY: y, rx: rx, ry: ry, building: b).save()
+                }
+                if (x == -4 && y == 5){
+                    b = new Building(type: BuildingType.findByDirname('road-south-east'))
+                    b?.init()
+                    new Field(coordX: x, coordY: y, rx: rx, ry: ry, building: b).save()
+                }
 
             }
         }
