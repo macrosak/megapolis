da23d2ed5b8e3505c1b98e61586e625669a6cdbb
Martin Hanak <hanakus@seznam.cz>
2011-04-27 13:51:46 +0200

    Field factory

--- grails-app/domain/com/megapolis/game/Field.groovy
+++ grails-app/domain/com/megapolis/game/Field.groovy
@@ -20,4 +20,17 @@ class Field {
     int price() {
         return 200;
     }
+
+
+    static createField(int x, int y)
+    {
+        double theta = Math.PI/4
+        def R = [Math.cos(theta), -Math.sin(theta), Math.sin(theta), Math.cos(theta)] as double[]
+
+        double rx = R[0] * x + R[1] * y
+        double ry = R[2] * x + R[3] * y
+
+        return new Field(coordX: x, coordY: y, rx: rx, ry: ry/*, building: background*/).save()
+
+    }
 }
