f789856a62b787f9172617535204ae9417bff88a
Macros <mknizek@gmail.com>
2011-05-08 15:41:31 +0200

    Added money for new player

--- grails-app/services/com/megapolis/PlayerService.groovy
+++ grails-app/services/com/megapolis/PlayerService.groovy
@@ -22,7 +22,7 @@ package com.megapolis
 import com.megapolis.game.player.Player
 
 class PlayerService {
-    static final int INITIAL_MONEY = 200000
+    static final int INITIAL_MONEY = 400000
 
     def newPlayer(id, profileJSON) {
         new Player(facebookId: id, profileJSON: profileJSON, money: INITIAL_MONEY).save()
